92ae5240c817b2134fdbc829579308bf391a398e
Roman Kříž <samnung@gmail.com>
2012-01-25 23:46:13 +0100

    experiment commit

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -26,6 +26,11 @@ MainWindow::MainWindow(files_control *files, int _window_number) : /*QMainWindow
 	ui->skutecny_label->hide();
 	ui->procento->hide();
 
+
+
+
+
+
 	ui->poplatek_line->setText("0,00");
 
 	//********** doplnovani comboboxu ************
